dc486fd1a6b83a916e42b8620da7f79ccac67e0e
Eduardo <choboellobo@gmail.com>
2017-06-08 19:39:42 +0200

    filter user GET / first_name and last_name

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -17,9 +17,17 @@ module.exports = function (app) {
 */
 router.get('/', function (req, res, next) {
   var query = {};
-  var options = {select: {password: 0}};
-    if(req.query.first_name) query.first_name = new RegExp(req.query.first_name)
+  var options = { select: {password: 0} };
+    if(req.query.first_name) {
+      query.$or = query.$or || []
+      query.$or.push({ first_name: new RegExp(req.query.first_name) })
+    }
+    if(req.query.last_name) {
+      query.$or = query.$or || []
+      query.$or.push({ last_name: new RegExp(req.query.last_name) })
+    }
     if(req.query.page) options.page = req.query.page
+
     User.paginate(query,options)
     .then(
       users => res.status(200).json(users)
@@ -36,7 +44,6 @@ router.get('/', function (req, res, next) {
 */
 router.post("/", function(req, res, next){
     let user = req.body;
-    console.log(user)
     if(!user) return res.status(403).json({error : true, message: "Data empty"})
     User.findOne({email: user.email})
         .then(userFound => {
