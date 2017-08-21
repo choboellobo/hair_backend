4c5a694b211a070b2b7f46d8de9e81f9e7531ab8
Eduardo <choboellobo@gmail.com>
2017-08-21 11:56:09 +0200

    filter view seo DONE

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -21,12 +21,12 @@ router.get('/', function (req, res, next) {
     Professional.find(
       query,
       {slug: 1, avatar: 1, first_name: 1, last_name: 1, background: 1, gender: 1, services: 1, options: 1, description: 1, phone: 1, payments: 1})
-      .sort({payments: -1})
+      .sort({'payments.plan': -1, 'created_at': 1})
       .populate('services')
       .then(
         professionals => {
           Service.find().then(
-            services => res.render('results', {professionals: professionals, services: services, query: req.query})
+            services => res.render('results', { professionals: professionals, services: services, query: req.query})
           )
         }
       )
@@ -52,3 +52,33 @@ router.get('/:slug', function (req, res, next) {
     error => next()
   );
 });
+
+/** CUSTOMER FILTERS ***/
+router.get('/busco/:query', function(req, res, next){
+  let what = req.params.query.split("-");
+  let location = what[2]
+  let profession = what[0]
+  let regExpLocation = new RegExp(location, 'i')
+  let regExpProfession = new RegExp(profession, 'i')
+  if(what[1] != 'en') return next()
+  Service.findOne({'professions': regExpProfession})
+  .then(
+    service => {
+      if(service == null) return next()
+      Professional.find(
+        {'address.location': regExpLocation, services: service._id},
+        {slug: 1, avatar: 1, first_name: 1, last_name: 1, background: 1, gender: 1, services: 1, options: 1, description: 1, phone: 1, payments: 1})
+        .sort({'payments.plan': -1, 'created_at': 1})
+        .populate('services')
+        .then(professionals => {
+          Service.find().then(
+            services => {
+              if(professionals.length == 0) return next();
+              res.render('results', {seo: `${profession.charAt(0).toUpperCase() + profession.slice(1)} ${what[1]} ${location.charAt(0).toUpperCase() + location.slice(1)}`,professionals: professionals, services: services, query: {w:what[2]}})
+            }
+          )
+        })
+    }
+  )
+
+})
