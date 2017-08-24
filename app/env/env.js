6a4fdd9c52c0d5ea7f81365eb38b9c151a6847ef
Eduardo <choboellobo@gmail.com>
2017-08-24 12:07:09 +0200

    hot fix

--- app/env/env.js
+++ app/env/env.js
@@ -3,7 +3,7 @@ module.exports = {
   host: process.env.HOST || "http://localhost:3000",
   jwt_key : 'wtf',
   crypto_key: 'wtf',
-	stripe_key: process.env.STRIPE_KEY || 'sk_live_mlZME2zMvnLHlkMsHenDi21v',
+	stripe_key: process.env.STRIPE_KEY || 'sk_test_MrFhxIb2j4Ebc6gBbq3GuHq7',
   cloudinary: {
     cloud_name: process.env.CLOUDINARY_CLOUDNAME || 'dyfof1mr5',
     api_key: process.env.CLOUDINARY_APIKEY || '156183584475275',
